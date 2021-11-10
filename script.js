cf0ec59c466f6061bbdaf11337da8ff1368f1729
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2021-11-10 16:01:19 +0530

    First upload
    most likely final version of a assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+function nameTeller(id){
+		alert("This is "+id);
+}
+function guider(id){
+		alert("This leads to "+id);
+}
+window.alert("This web page is only synchronised for desktop usage only")
